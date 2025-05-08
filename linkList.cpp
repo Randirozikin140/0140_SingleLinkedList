18ef4bf3d27ea657e5c1feeed62503cbb325342a
Randi rozikin <randi.roziikin.ft24@mail.umy.ac.id>
2025-05-08 21:50:43 +0700

    perbaikan isi dari program

--- linkList.cpp
+++ linkList.cpp
@@ -80,7 +80,7 @@ public:
     {
         Node *current, *previous;
         if (!search(nim, &previous, &current))
-        return;
+        
 
         if (current == START)
             START = START->next;
@@ -88,7 +88,7 @@ public:
             previous->next = current->next;
 
         delete current;
-        return;
+        return true;
     }
 
     void traverse()
@@ -117,4 +117,102 @@ int main()
     List mhs;
     int nim;
     char ch;
-}
+
+    do
+    {
+        cout << "Menu" << endl;
+        cout << "1. Menambah data kedalam list" << endl;
+        cout << "2. Menghapus data dari list" << endl;
+        cout << "3. Menampilkan semua data dari list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar" << endl;
+        cout << endl;
+        cout<< "Masukkanpilihan (1-5): ";
+        cin >> ch;
+
+        switch (ch)
+        {
+            case '1':
+            {
+                mhs.addNode();
+                break;
+            
+
+                case '2':
+                {
+                    if (mhs.listEmpty())
+                    {
+                        cout << endl;
+                        cout<< "List kosong" << endl;
+                        break;
+                    }
+                    else
+                    {
+                        cout << endl;
+                        cout << "\nMasukkan no mahasiswa yang akan dihapus : ";
+                        cin >> nim;
+                    }
+
+                    if (mhs.delNode(nim) == false)
+                    {
+                            cout << endl;
+                            cout << "Data tidak ditemukan" << endl;
+                    }
+                    else
+                    {
+                        cout << endl;
+                        cout << "Data dengan nomor mahasiswa" << nim << " berhasil di hapus " << endl;
+                    }
+                }
+                break;
+            }
+            case '3':
+                
+                mhs.traverse();
+                break;
+
+            case '4':
+            {
+                if (mhs.listEmpty())
+                {                        
+                    cout << "\nList Kosong\n";
+                    break;
+                }
+                else
+                {
+                    Node *previous, *current;
+                    cout << endl;
+                    cout << "Masukkan no mahasiswa yang dicari : ";
+                    cin >> nim;
+
+                    if (mhs.search(nim, &previous, &current) == false)
+                    {
+                        cout << endl;
+                        cout << "Data tidak ditemukan" << endl;     
+                    }
+                    else
+                    {
+                    cout << endl;
+                    cout << "Data ditemukan" << endl;
+                    cout << "\nNo Mahasiswa: " << current->noMhs << endl;
+                    cout << "\n";
+                    }
+                }
+                break;
+                case '5':
+                {
+
+                }
+                break;
+                default:
+                {
+                    cout << "Pilihan salah !." << endl;
+                }
+                break;
+                    
+            }
+        }
+    } while (ch != '5');
+    return 0;
+};
+   

0d518d5f213b64a800c15ff8340d40868aff9541
Randi rozikin <randi.roziikin.ft24@mail.umy.ac.id>
2025-05-08 15:15:45 +0700

    Program LinkLList

---
+++ linkList.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Node 
+{
+public:
+    int noMhs;
+    Node *next;
+};
+
+class List
+{
+    Node *START;
+
+public:
+    List()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+
+        }
+    }
+};
+
